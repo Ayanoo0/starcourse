a42786aace73197014dd1338c586dbecd7885b3d
Ayanoo0 <897546696@qq.com>
2023-03-06 22:53:06 +0800

    练习1.3-1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,75 +1,83 @@
 // 定义了一个名为App的React组件
-const Header = (prop) => {
-  return (
-    <div>
-      <h1>{prop.course}</h1>
-    </div>
-  );
-};
-const Part1 = (prop) => {
-  return (
-    <div>
-      {prop.part1} {prop.exercises1}
-    </div>
-  );
-};
-const Part2 = (prop) => {
-  return (
-    <div>
-      {prop.part2} {prop.exercises2}
-    </div>
-  );
-};
-const Part3 = (prop) => {
-  return (
-    <div>
-      {prop.part3} {prop.exercises3}
-    </div>
-  );
-};
-const Content = (prop) => {
-  return (
-    <div>
-      <Part1 part1={prop.part1} exercises1={prop.exercises1} />
-      <Part2 part2={prop.part2} exercises2={prop.exercises2} />
-      <Part3 part3={prop.part3} exercises3={prop.exercises3} />
-    </div>
-  );
-};
-const Total = (prop) => {
-  return (
-    <div>
-      <p>
-        Number of exercises{" "}
-        {prop.exercises1 + prop.exercises2 + prop.exercises3}
-      </p>
-    </div>
-  );
-};
+// const Header = (prop) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>{prop.course}</h1>
+//     </div>
+//   );
+// };
+// const Part1 = (prop) => {
+//   return (
+//     <div>
+//       {prop.part1} {prop.exercises1}
+//     </div>
+//   );
+// };
+// const Part2 = (prop) => {
+//   return (
+//     <div>
+//       {prop.part2} {prop.exercises2}
+//     </div>
+//   );
+// };
+// const Part3 = (prop) => {
+//   return (
+//     <div>
+//       {prop.part3} {prop.exercises3}
+//     </div>
+//   );
+// };
+// const Content = (prop) => {
+//   return (
+//     <div>
+//       <Part1 part1={prop.part1} exercises1={prop.exercises1} />
+//       <Part2 part2={prop.part2} exercises2={prop.exercises2} />
+//       <Part3 part3={prop.part3} exercises3={prop.exercises3} />
+//     </div>
+//   );
+// };
+// const Total = (prop) => {
+//   return (
+//     <div>
+//       <p>
+//         Number of exercises{" "}
+//         {prop.exercises1 + prop.exercises2 + prop.exercises3}
+//       </p>
+//     </div>
+//   );
+// };
+
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of componet";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <p>{course.name}</p>
+      <p>
+        {course.parts[0].name} {course.parts[0].exercises}
+      </p>
+      <p>
+        {course.parts[1].name} {course.parts[1].exercises}
+      </p>
+      <p>
+        {course.parts[1].name} {course.parts[1].exercises}
+      </p>
     </div>
   );
 };
