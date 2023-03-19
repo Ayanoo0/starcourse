bff558a3302824a7f4312c778f2d448761f7b9ca
Ayanoo0 <897546696@qq.com>
2023-03-19 18:35:37 +0800

    test

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+let counter = 1;
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App counter={counter} />
+  );
+};
+
+const refreshAdd = () => {
+  refresh();
+  counter += 1;
+};
+setInterval(refreshAdd, 2000);
